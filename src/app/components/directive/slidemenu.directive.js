349fa0700b7af5ad3ba054670365772f4221fe4a
gaoyangxiaozhu <gyyzyp@163.com>
2016-03-31 16:19:18 +0800

    add slidemenu.directive.js

---
+++ src/app/components/directive/slidemenu.directive.js
@@ -0,0 +1,32 @@
+(function () {
+   'use strict';
+
+   angular.module('gzblog.directives')
+       .directive('slideMenu',function ($window) {
+           return {
+               restrict:'EA',
+               link:function (scope, element, attrs) {
+                   var that = element;
+                   scope.$watch(scope.isLoggedIn, function(){
+                     var mainLinks = that.find('.navbar-item');
+                     var slideBox = that.siblings('.slider-box');
+                     var width = that.find('li:first').width();
+
+                     mainLinks.each(function(){
+                        var _ = $(this);
+                        var parent = _.parents('li');
+                        var left = parent.position().left +width+'px';
+                        _.click(function(){
+                          if(!_.hasClass('active')){
+                            mainLinks.removeClass('active');
+                            _.addClass('active');
+                          }
+                           slideBox.css({'left': left});
+                        });
+                     });
+
+                   });
+               },
+           };
+       });
+})();
